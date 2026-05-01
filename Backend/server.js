bcbd38c897b0ba0305fe2e13f0fbe7538bc171fd
Tejasi Gupta <bharat16choudhary@gmail.com>
2026-05-01 13:31:54 +0530

    restructured project into frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../Frontend/public')));
 
 // EJS Setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../Frontend/views'));
 
 // Session Config
 app.use(session({
